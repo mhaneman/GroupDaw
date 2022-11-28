8da62db2359dfcd3ae3e76dd1a4205a68e87084b
michaelhaneman <mhaneman@calpoly.edu>
2022-11-28 01:49:54 -0800

    functional piano roll

--- src/PianoRoll.tsx
+++ src/PianoRoll.tsx
@@ -10,26 +10,25 @@ import useWindowDimensions from './hooks/useWindowDimensions';
 import Keyboard from './Keyboard';
 
 // TODO 
-
 // 1.
 // create boxes than can be resized on the right side -- done
 // these boxes are draggable when selected in the middle -- done
 
 // 2. 
-// create grid -- kinda 
-// add a piano on the side -- in progress
+// create grid -- done 
+// add a piano on the side -- done
 
 // 3
 // these boxes snap to quarter note sections -- kinda
-// double click to add a new box
+// double click to add a new note -- done
 
 // 4
-// export the note pitch and time
+// schedule notes to play right pitch and time
 
 export default function PianoRoll({numOfSteps = 16}) {
 
   var channel = new Tone.Channel().toDestination();
-  channel.volume.value = Tone.gainToDb(Number(0.5));
+  channel.volume.value = Tone.gainToDb(Number(0.3));
 
   const sampler = new Tone.Sampler({
     urls: {
@@ -53,13 +52,11 @@ export default function PianoRoll({numOfSteps = 16}) {
   const handleAddNewNote = (event) => {
     const local_x = event.clientX - event.target.offsetLeft;
     const local_y = event.clientY - event.target.offsetTop;
-    console.log(local_x, local_y);
     setNotes([...notes, {x:local_x, y:local_y}]);
   }
 
   return (
     <div className="" onDoubleClick={handleAddNewNote}>
-      <Measures />
       <Resizable className={styles.piano_roll} style={{position: 'relative', overflow: 'auto', padding: '0'}}
         size={{ width: area.width, height: area.height }}
         onResizeStop={(e, direction, ref, d) => {
@@ -70,65 +67,72 @@ export default function PianoRoll({numOfSteps = 16}) {
         }}>
         <Keyboard />
         <div style={bc_style}>
-          {notes.map(() => <Note />)}
+          {notes.map(() => <Note instr={sampler}/>)}
         </div>
       </Resizable>
     </div>
   )
 }
 
-function Measures() {
-  return (
-    <div>
-      Measures
-    </div>);
-}
 
-
-// change this to a class --> get property of position
-// with the get property of position --> return a note value
-// with the get property of position --> return a time
 // double click --> remove this note
-
-function Note() {
-  const [area, setArea] = useState({width: 320, height: 25});
+// need to convert note pos to absolute position
+function Note({instr}) {
   
-  const [state, setState] = useState({
-    deltaPosition: {
-      x: 0, y: 0
-    },
-  });
+  // need to create an absolute position
+  const [pos, setPos] = useState({x:0, y:0});
+  const [area, setArea] = useState({width:400, height: 25});
+
+  const noteRef = React.useRef<Tone.ToneEvent | null>(null); 
+
+  useEffect(() => {
+
+    // need to convert y-pos to pitch
+    const pitches = ["C3", "C#3", "D3", "D#3", "E3", "F3", "F#3", "G3", "G#3", "A3", "A#3", "B3"];
+    var pitch = pitches.at(pos.y / 25);
+
+    noteRef.current = new Tone.ToneEvent(((time, chord) => {
+      instr.triggerAttackRelease(chord, 0.5, time);
+    }), pitch);
+
+    const dev = Math.pow(Tone.Transport.bpm.value / 60, -1) / (16 * 25); // converts x-pos to time
+    noteRef.current.start(pos.x * dev);
+    // noteRef.current.loop = 8;
+    noteRef.current.loop = true;
+    noteRef.current.loopEnd = "1m";
+
+    return () => {
+      noteRef.current?.dispose();
+    };
+  }, [pos]);
 
   const handleDrag = (e, ui) => {
-    const {x, y} = state.deltaPosition;
-    setState({
-      deltaPosition: {
+  const {x, y} = pos;
+    setPos({
         x: x + ui.deltaX,
         y: y + ui.deltaY,
-      }
     });
   };
 
-  const {deltaPosition } = state;
-    
-  return (
-  <Draggable handle="#handle" grid={[25, 25]} bounds="parent" position={{x: 200, y:200}} onDrag={handleDrag}>
-    <Resizable
-      size={{ width: area.width, height: area.height }}
-      style={{border: "1px solid black"}}
-      onResizeStop={(e, direction, ref, d) => {
-        setArea({
-          width: area.width + d.width,
-          height: area.height,
-        });
-      }}
-      >
-        <div id="handle" style={{backgroundColor: "blue", color:"white"}}>
-          Sample note -- x: {state.deltaPosition.x.toFixed(0)}, y: {state.deltaPosition.y.toFixed(0)}
-        </div>
-      </Resizable>
-  </Draggable>
-  )
+  const handleArea = (d) => {
+    setArea({
+        width: area.width + d.width,
+        height: area.height
+    });
+  }
 
+  return (
+    <Draggable handle="#handle" grid={[25, 25]} bounds="parent" defaultPosition={{x:0, y: 0}} onDrag={handleDrag}>
+      <Resizable
+        size={{ width: area.width, height: area.height }}
+        style={{border: "1px solid black"}}
+        onResizeStop={(e, direction, ref, d) => handleArea(d)}>
+          
+          <div id="handle" style={{backgroundColor: "#25838a", color:"white"}}>
+            Sample note -- x: {pos.x.toFixed(0)}, y: {pos.y.toFixed(0)}
+          </div>
+        </Resizable>
+    </Draggable>
+    );
 }
 
